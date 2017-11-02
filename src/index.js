d701aaf42750963c80139963b9c84daad50aacfd
Anton Bright <orthantbing@live.com>
2017-11-03 00:23:34 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 
+import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
 import App from "./App";
 import reducers from "./reducers";
